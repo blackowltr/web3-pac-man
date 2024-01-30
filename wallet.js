6184b8284032d176ab24d05f757b76f7c769040a
BlackOwl <107190154+blackowltr@users.noreply.github.com>
2024-01-30 09:12:40 +0300

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -1,43 +1,66 @@
-const getProvider = () => {
-    if ('ethereum' in window) { // 'phantom' yerine 'ethereum' kullanıyoruz
-        const provider = window.ethereum;
+const connectToArtelaTestNetAutomatically = async () => {
+    try {
+        const networkName = 'Artela TestNet';
+        const rpcUrl = 'https://betanet-rpc1.artela.network';
+        const chainId = '11822';
+        const symbol = 'ART';
+        const blockExplorerUrl = 'https://betanet-scan.artela.network';
 
-        if (provider.isMetaMask) { // 'isPhantom' yerine 'isMetaMask' kullanıyoruz
-            return provider;
-        }
+        // Bağlantı bilgilerini kullanarak ağa bağlanma işlemi
+        const provider = new Web3.providers.HttpProvider(rpcUrl);
+        const web3 = new Web3(provider);
+
+        // Ağa başarıyla bağlanıldı mesajını gösterme
+        console.log(`Connected to ${networkName} TestNet`);
+
+        // İsteğe bağlı olarak diğer bilgileri de kullanabilirsiniz
+        console.log('ChainID:', chainId);
+        console.log('Symbol:', symbol);
+        console.log('Block Explorer URL:', blockExplorerUrl);
+
+        // Bağlantıyı döndürme
+        return web3;
+    } catch (err) {
+        console.error(err);
     }
+};
 
-    // Metamask bulunamadıysa, kullanıcıyı yönlendir veya başka bir şey yapabilirsiniz.
-    // Örneğin, kullanıcıyı Metamask indirmeye yönlendirme:
-    // window.open('https://metamask.io/download.html', '_blank');
+const getProvider = async () => {
+    try {
+        const web3 = await connectToArtelaTestNetAutomatically();
+        return web3.currentProvider;
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 const connectWallet = async () => {
-    const provider = getProvider();
+    const provider = await getProvider();
     try {
         // Metamask bağlantısını sağlama
         await provider.request({ method: "eth_requestAccounts" });
     } catch (err) {
         console.error(err);
     }
-}
+};
 
 const requestWalletConnection = async () => {
-    const provider = getProvider();
+    const provider = await getProvider();
     try {
         // Metamask bağlantı talebini sağlama
         await provider.request({ method: "eth_requestAccounts" });
     } catch (err) {
         console.error(err);
     }
-}
+};
 
-const getWalletAddress = () => {
-    const provider = getProvider();
+const getWalletAddress = async () => {
+    const provider = await getProvider();
     try {
         // Metamask'tan cüzdan adresini al
-        return provider?.selectedAddress || '';
+        const accounts = await provider.request({ method: "eth_accounts" });
+        return accounts[0] || '';
     } catch (err) {
         return '';
     }
-}
+};
